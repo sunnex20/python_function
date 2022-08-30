29dc173efc988141f8d1609e6409a2ffa65b3b2c
jack234 <jacksunday197@gmail.com>
2022-08-30 16:02:12 +0100

    assignment on py1

---
+++ python_function.py
@@ -0,0 +1,8 @@
+def greet (first_name, last_name):
+    print (f'Hi {first_name} {last_name }')
+    print("wecome aboard")
+    print("am single")
+    print("but i love u ")
+    print('pleas come back home ur family really need u')
+    
+greet('john','peter')
